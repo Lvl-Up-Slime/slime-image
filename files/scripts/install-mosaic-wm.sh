ebc738f3bd008341f496b27282303f5e8ace1229
Lvl-Up_Slime <angelcabreraxo@gmail.com>
2026-08-27 23:49:20 -0400

    2nd try to install mosaic...

--- files/scripts/install-mosaic-wm.sh
+++ files/scripts/install-mosaic-wm.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+UUID="mosaicwm@cleomenezesjr.github.io"
+
 git clone https://github.com/CleoMenezesJr/MosaicWM.git /tmp/MosaicWM
 cd /tmp/MosaicWM
 
-# Flip DEBUG off before building, per project's own recommendation
+grep -q 'const DEBUG = true;' extension/logger.js
 sed -i 's/const DEBUG = true;/const DEBUG = false;/' extension/logger.js
 
-./scripts/build.sh -i
+./scripts/build.sh -b
 
+mkdir -p "/usr/share/gnome-shell/extensions/$UUID"
+unzip -o "$UUID.zip" -d "/usr/share/gnome-shell/extensions/$UUID"
+
+if [ -d "/usr/share/gnome-shell/extensions/$UUID/schemas" ]; then
+    glib-compile-schemas "/usr/share/gnome-shell/extensions/$UUID/schemas"
+fi
+
+cd /
 rm -rf /tmp/MosaicWM
